13248517e27c44c7c7e947a686fab07f0011479a
Gabriel Hess <heg2@bfh.ch>
2023-12-21 14:43:40 +0100

    use folders (something went wrong during copying the files)

--- index.ts
+++
@@ -1,7 +0,0 @@
-import de from './de';
-import fr from './fr';
-
-export default {
-  de: de,
-  fr: fr
-};
--- services/keys.ts
+++
@@ -1,3 +0,0 @@
-import {Utils} from 'src/models/types';
-import {InjectionKey} from 'vue';
-export const utilsKey: InjectionKey<Utils> = Symbol('utilsKey');
