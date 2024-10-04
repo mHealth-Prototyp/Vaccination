670169046f54ce48ac434f31de6c02c8d4b25b31
heg2 <heg2@bots.github.com>
2024-10-04 07:48:09 +0000

    updated GitHub Pages

---
+++ assets/composables.c9f79b03.js
@@ -0,0 +1,1 @@
+import{i as r,ap as t}from"./index.9b44893e.js";function s(){const e=r(t);if(!e)throw new Error("Key was not provided.");return e}export{s as u};
---
+++ assets/use-quasar.298dd09d.js
@@ -0,0 +1,1 @@
+import{i as a,aa as r}from"./index.9b44893e.js";function u(){return a(r)}export{u};
